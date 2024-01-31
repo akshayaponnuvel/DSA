4e44e31ac1f155fe3c344ffc168ed6547104a295
Akshaya Ponnuvel <akshayaponnuvel44@gmail.com>
2024-01-31 21:07:28 +0530

    added some files

---
+++ Search_Algorithms/LinearSearch.java
@@ -0,0 +1,33 @@
+package Search_Algorithms;
+
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int[] arr={7,18,29,41,52,63,74};
+        int target=29;
+
+        int ans=Search(arr , target);
+
+        if(ans==-1){
+            System.out.println("Element not found");
+        }
+        else{
+            System.out.println("Element found at index "+ ans);
+        }
+    }
+
+    private static int Search(int[] arr, int target) {
+        if(arr.length==0){
+            return -1;
+        }
+        else{
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]==target){
+                    return i;
+                }
+            }
+        }
+        return-1;
+    }
+}
