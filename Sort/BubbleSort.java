54d78b875b71bf99c48938063d8bf95efd7cc380
Akshaya Ponnuvel <akshayaponnuvel44@gmail.com>
2024-02-01 21:59:16 +0530

    added bubble sort

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,30 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr={9,8,7,6,5,4,3,2,1};
+
+        sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void sort(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    swap(arr, j,j-1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr,int i, int j) {
+        int temp= arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
