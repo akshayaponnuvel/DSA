08246edbe0a753e26d6dc83ebc5c9a3d4b9f3566
Akshaya Ponnuvel <akshayaponnuvel44@gmail.com>
2024-02-03 21:02:57 +0530

    added counting zeroes in number using recursion

---
+++ Recursion/CountZeroes.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class CountZeroes{
+
+    public static void main(String[] args){
+
+        int n=10203;
+
+        int count=count(n,0);
+
+        System.out.println(count);
+    }
+
+    private static int count(int n,int count) {
+        if(n==0){
+            return count;
+        }
+        if(n%10==0){
+            count++;
+        }
+        return count(n/10,count);
+    }
+}
