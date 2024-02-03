c195f4f329b56439fabd6bf2ad40e090a35c4168
Akshaya Ponnuvel <akshayaponnuvel44@gmail.com>
2024-02-03 14:49:54 +0530

    added sum of digits using recursion

--- Recursion/SumOfDigits.java
+++ Recursion/SumOfDigits.java
@@ -13,14 +13,9 @@ public class SumOfDigits {
     }
 
     private static int sum(int n) {
-        int ans;
-        if(n>0 && n<=1){
-            ans=n;
+        if (n<=9) {
             return n;
         }
-
-        ans=n+sum(n-1);
-        
-        return ans;
+        return sum(n%10)+sum(n/10);
     }
 }
