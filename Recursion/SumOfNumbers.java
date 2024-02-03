d28f66f198cf671d9cc42f4e3a2c92550c743cf8
Akshaya Ponnuvel <akshayaponnuvel44@gmail.com>
2024-02-03 14:32:39 +0530

    added sum of n numbers using recursion

---
+++ Recursion/SumOfNumbers.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int ans=sum(n);
+        System.out.println(ans);
+    }
+
+    private static int sum(int n) {
+        int ans;
+        if(n>0 && n<=1){
+            ans=n;
+            return n;
+        }
+
+        ans=n+sum(n-1);
+        
+        return ans;
+    }
+}
