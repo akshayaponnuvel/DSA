d1431746e0f8a39053769bddab9ef1cd033ac4be
Akshaya Ponnuvel <akshayaponnuvel44@gmail.com>
2024-02-09 07:04:31 +0530

    updated

--- Custom/LinkedList/CustomLinkedList.java
+++ Custom/LinkedList/CustomLinkedList.java
@@ -2,6 +2,65 @@ package Custom.LinkedList;
 
 
 public class CustomLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int size;
     
-   
+    public CustomLinkedList() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int value){
+        Node node=new Node(value);
+        node.next=head;
+        head=node;
+
+        if(tail==null){
+            tail=head;
+        }
+
+        size++;
+    }
+
+    public void insertLast(int value){
+        if(tail==null){
+            insertLast(value);
+            return;
+        }
+        Node node=new Node(value);
+        node.next=tail;
+        tail=node;
+
+        size++;
+    }
+
+    public void display(){
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.value + " -> ");
+            temp=temp.next;
+        } 
+        System.out.println("END");
+    }
+
+    private class Node{
+
+        private int value;
+        private Node next;
+        
+        
+        public Node(int value) {
+            this.value = value;
+        }
+    
+    
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+        
+    }
 }
+
+
---
+++ Custom/LinkedList/Main.java
@@ -0,0 +1,18 @@
+package Custom.LinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        CustomLinkedList list=new CustomLinkedList();
+
+
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+
+        list.display();
+        
+    }
+
+}
